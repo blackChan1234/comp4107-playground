81ee5fe146a5354c0fbd78d70662700186db8108
f4218073 <f4218073@csdlabad.local>
2025-02-27 13:25:06 +0800

    Add self-introduction feature

--- src/main/kotlin/HermesChan/SelfIntroduction.kt
+++ src/main/kotlin/HermesChan/SelfIntroduction.kt
@@ -2,7 +2,7 @@ package hk.edu.hkbu.comp.comp4107.playground.HermesChan
 
 class SelfIntroduction {
     fun selfIntroduction() {
-        println("Hello, my name is Kenny Cheng.")
+        println("Hello, my name is Hermes Chan.")
         println("I am a student at HKBU.")
         println("I am studying Computer Science.")
         println("I am interested in machine learning.")
